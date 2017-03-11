14a0af3e2083932b448adbf35d0a913e23ab3a2f
Sean <seanmfoster@users.noreply.github.com>
2017-03-11 11:17:44 -0500

    Typed a Hello World message

--- src/main/java/com/rhymeknowreason/HelloWorld.java
+++ src/main/java/com/rhymeknowreason/HelloWorld.java
@@ -3,5 +3,6 @@ package com.rhymeknowreason;
 public class HelloWorld {
     public static void main(String[] args) {
         // TODO; Print "Hello World!" (without the quotes) to the terminal here.
+        System.out.println ("Hello World!");
     }
 }
